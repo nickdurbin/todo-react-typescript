8ddefe0f198a8c284cbf21fadfee22238433d4b5
ndurbin <nick.durbin@gmail.com>
2020-02-16 13:56:01 -0500

    Simple input to accept new todos

---
+++ src/components/Todos/TodoForm.tsx
@@ -0,0 +1,32 @@
+import React, { useState, ChangeEvent, FormEvent } from "react";
+
+interface AddTodoFormProps {
+  addTodo: AddTodo;
+}
+
+const TodoForm: React.FC<AddTodoFormProps> = ({ addTodo }) => {
+  const [newTodo, setNewTodo] = useState<string>("")
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTodo(e.target.value)
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    addTodo(newTodo)
+    setNewTodo("")
+  }
+ 
+  return (
+    <form >
+      <input type="text"
+      value={newTodo}
+      onChange={handleChange} />
+      <button type="submit" onClick={handleSubmit}>
+        Add Todo
+      </button>
+    </form>
+  )
+}
+
+export default TodoForm;
