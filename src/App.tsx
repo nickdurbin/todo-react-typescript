6ba54c217345bade7a789bb968c402310c515814
ndurbin <nick.durbin@gmail.com>
2020-02-16 13:55:35 -0500

    initial state, toggle complete, addTodo, TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './components/Todos/TodoList';
+import TodoForm from './components/Todos/TodoForm';
+
+const initialTodos: Array<Todo> = [
+  { text: "Walk the dog.", complete: true },
+  { text: "Write application.", complete: false }
+]
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos)
+
+  const toggleTodo: ToggleTodo = selectedTodo => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete
+        }
+      }
+      return todo;
+    })
+    setTodos(newTodos)
+  }
+
+  const addTodo: AddTodo = newTodo => {
+    //trim() does not equal an empty string
+    // keeps from entering empty strings in the app
+    newTodo.trim() !== "" && 
+      setTodos([ ...todos, {text: newTodo, complete: false } ])
+  }
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <TodoForm addTodo={addTodo}/>
+    </>
   );
 }
 
